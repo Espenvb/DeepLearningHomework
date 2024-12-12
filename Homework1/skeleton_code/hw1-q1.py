2423c6cff759c3add7d6e892fe6fed9d3de9e512
Fredrick Carlsåker <carlsaker@live.se>
2024-12-12 15:08:45 +0100

    Q1 freds

--- Homework1/skeleton_code/hw1-q1.py
+++ Homework1/skeleton_code/hw1-q1.py
@@ -31,13 +31,12 @@ class LinearModel(object):
     def evaluate(self, X, y):
         """
         X (n_examples x n_features)
-        y (n_examples): gold labels
+        y (n_examples): gold labels command: python hw1-q1.py perceptron -epochs 100
         """
-        y_hat = self.predict(X)
+        y_hat = self.predict(X)         
         n_correct = (y == y_hat).sum()
         n_possible = y.shape[0]
         return n_correct / n_possible
-
 
 class Perceptron(LinearModel):
     def update_weight(self, x_i, y_i, **kwargs):
@@ -46,9 +45,12 @@ class Perceptron(LinearModel):
         y_i (scalar): the gold label for that example
         other arguments are ignored
         """
-        raise NotImplementedError # Q1.1 (a)
-
-
+        predicted_label = self.predict(x_i) 
+        if predicted_label != y_i:
+            self.W[y_i] +=  x_i
+            self.W[predicted_label] -= x_i
+        return
+    
 class LogisticRegression(LinearModel):
     def update_weight(self, x_i, y_i, learning_rate=0.001, l2_penalty=0.0, **kwargs):
         """
@@ -56,18 +58,36 @@ class LogisticRegression(LinearModel):
         y_i: the gold label for that example
         learning_rate (float): keep it at the default value for your plots
         """
-        raise NotImplementedError # Q1.2 (a,b)
+        scores = np.dot(self.W, x_i)                        # (n_classes)
+        exp_scores = np.exp(scores - np.max(scores))        # (n_classes)
+        probabilities = exp_scores / np.sum(exp_scores)     # (n_classes)
+        one_hot_y = np.zeros(self.W.shape[0])               # (n_classes)
+        one_hot_y[y_i] = 1
 
+        gradients = np.outer((probabilities - one_hot_y), x_i)
+        gradients += l2_penalty * self.W
 
+        self.W -= learning_rate * gradients
+        
 class MLP(object):
-    def __init__(self, n_classes, n_features, hidden_size):
+    def __init__(self, n_classes, n_features, hidden_size=100):
         # Initialize an MLP with a single hidden layer.
-        raise NotImplementedError # Q1.3 (a)
+
+        self.W_1 = np.random.normal(loc=0.1, scale=0.1, size=(hidden_size, n_features)) 
+        self.b_1 = np.zeros((hidden_size,1))
+        self.W_2 = np.random.normal(loc=0.1, scale=0.1, size=(n_classes, hidden_size))
+        self.b_2 = np.zeros((n_classes,1))
 
     def predict(self, X):
         # Compute the forward pass of the network. At prediction time, there is
         # no need to save the values of hidden nodes.
-        raise NotImplementedError # Q1.3 (a)
+        z = np.dot(self.W_1, np.transpose(X)) + self.b_1            # size (hidden_size, n_examles), each column is an example, each row is a node
+        h = np.maximum(0, z)                                        # ReLu function of z, size(hidden_size, n_examples), eac
+        scores = np.dot(self.W_2, h) + self.b_2                     # weights to output layer, size(n_classes, n_examples), each row is one node
+        exp_scores = np.exp(scores - np.max(scores, axis=0))        # for stability, (n_classes, n_examples)
+        probabilities = exp_scores / np.sum(exp_scores, axis=0, keepdims=True)     # softmax, size (n_classes, n_examples)
+        predicted_labels = probabilities.argmax(axis=0)             # pick the largest prob. size (n_examples)
+        return predicted_labels
 
     def evaluate(self, X, y):
         """
@@ -80,11 +100,115 @@ class MLP(object):
         n_possible = y.shape[0]
         return n_correct / n_possible
 
+    """
+    def train_epoch(self, X, y, learning_rate=0.001, **kwargs):
+        """
+        #Dont forget to return the loss of the epoch.
+    """     
+        # shuffle the input
+        #indexes = np.arange(len(X))
+        #np.random.shuffle(indexes)
+        #X_shuffled = X[indexes]
+        #y_shuffled = y[indexes]
+        start_time = time.time()
+        # set up mini_batches, default=1
+        batch_size = 1
+        num_batches = len(X) // batch_size
+        loss = 0
+        #print('main for loop')
+
+        # do num_batches amount of gradient updates
+        for i in range(num_batches):
+            X_batch = X[i * batch_size : (i + 1) * batch_size]
+            y_batch = y[i * batch_size : (i + 1) * batch_size]
+
+        # predict
+            #print('predict)')
+            z = np.dot(self.W_1, np.transpose(X_batch)) + self.b_1                      # size (hidden_size, n_examles), each column is an example, each row is a node
+            h = np.maximum(0, z)                                                        # ReLu function of z, size(hidden_size, n_examples), eac
+            scores = np.dot(self.W_2, h) + self.b_2                                     # weights to output layer, size(n_classes, n_examples), each row is one node
+            exp_scores = np.exp(scores - np.max(scores, axis=0))                        # for stability, (n_classes, n_examples)
+            probabilities = exp_scores / np.sum(exp_scores, axis=0, keepdims=True)      # softmax, size (n_classes, n_examples)
+
+            # create one hot encoded matrix_like probabilities
+            one_hot = np.zeros_like(probabilities)                                      # (n_classes, n_examples)
+            one_hot[y_batch, np.arange(len(y_batch))] = 1
+
+            # compute negative log-likelihood loss function
+            eps = 1e-99                                                                 # for numerical stability
+            loss += np.sum(-np.log(probabilities[one_hot == 1] + eps))
+
+            # compute gradients
+            #print('compute gradients')
+            gradient_z2 = probabilities - one_hot                                       # dL/dz2, size(n_classes, n_examples) 
+
+            #gradient_b2 = np.sum(gradient_z2, axis=1, keepdims=True) / batch_size       # dL/db2 (n_classes, 1)
+            gradient_W2 = np.dot(gradient_z2, np.transpose(h)) / batch_size             # dL/dW2 (n_classes, hidden_size)
+            
+            #gradient_h1 = np.dot(np.transpose(self.W_2), gradient_z2)                   # dL/dh1 (hidden_size, n_examples)
+            gradient_z2 = np.dot(np.transpose(self.W_2), gradient_z2)                   # dL/dh1 (hidden_size, n_examples)
+
+            self.b_2 -= learning_rate * (np.sum(gradient_z2, axis=1, keepdims=True) / batch_size)       # dL/db2 (n_classes, 1)
+            self.W_2 -= learning_rate * (np.dot(gradient_z2, np.transpose(h)) / batch_size)             # dL/dW2 (n_classes, hidden_size)
+
+            gradient_relu = np.zeros_like(gradient_z2)                                  # dh1/dz (hidden_size, n_examples)
+            gradient_relu[h > 0] = 1
+            #gradient_z1 = gradient_z2 * gradient_relu                                   # dL/dz1 = dL/dh1 * dh1//dz1 (hidden_size, n_examples)
+            gradient_z1 = gradient_z2 * gradient_relu                                   # dL/dz1 = dL/dh1 * dh1//dz1 (hidden_size, n_examples)
+            
+            gradient_b1 = np.sum(gradient_z1, axis=1, keepdims=True) / batch_size       # dL/db1 (hidden_size,1)
+            gradient_W1 = np.dot(gradient_z1, X_batch) / batch_size                     # dL/dW1 (hidden_size, n_features)
+            
+            # update weights
+            self.W_2 -= learning_rate * gradient_W2
+            self.b_2 -= learning_rate * gradient_b2
+
+            self.W_1 -= learning_rate * gradient_W1
+            self.b_1 -= learning_rate * gradient_b1
+        endtime = time.time()
+        elapsed_time = endtime - start_time
+        print(f"Elapsed time: {elapsed_time:.2f} seconds")
+
+        return loss / (num_batches * batch_size)                                        # normalize by n_samples
+        """
+
     def train_epoch(self, X, y, learning_rate=0.001, **kwargs):
         """
         Dont forget to return the loss of the epoch.
-        """
-        raise NotImplementedError # Q1.3 (a)
+        """     
+        loss = 0
+        n_samples = X.shape[0]
+
+        # do n_samles amount of gradient updates
+        for idx in range(n_samples):
+            y_i = y[idx]
+            x_i = X[idx].reshape(-1, 1)
+
+        # predict
+            z = np.dot(self.W_1, x_i) + self.b_1                                        # size (hidden_size, n_examles)
+            h = np.maximum(0, z)                                                        # ReLu function of z, size(hidden_size, n_examples)
+            scores = np.dot(self.W_2, h) + self.b_2                                     # weights to output layer, size(n_classes, n_examples)
+            exp_scores = np.exp(scores - np.max(scores, axis=0, keepdims=True))         # for stability, (n_classes, n_examples)
+            probabilities = exp_scores / np.sum(exp_scores, axis=0, keepdims=True)      # softmax, size (n_classes, n_examples)
+
+            # compute negative log-likelihood loss function
+            loss += np.sum(-np.log(probabilities[y_i]))
+
+            # compute gradients of output layer
+            gradients_l2 = probabilities
+            gradients_l2[y_i] -= 1                                                      # dL/dz2, size(n_classes, 1) 
+
+            # compute gradients of input layer
+            gradients_l1 = np.dot(np.transpose(self.W_2), gradients_l2)                 # dL/dh1 (hidden_size, 1)
+            gradients_l1[z <= 0] = 0                                                    # dL/dz1 = dL/dh1 * dh1//dz1 (hidden_size, 1)
+
+            # update weights and biases
+            self.b_2 -= learning_rate * gradients_l2                                    # dL/db2 = dL/dz2
+            self.W_2 -= learning_rate * np.dot(gradients_l2, np.transpose(h))           # dL/dW2 (n_classes, hidden_size)
+
+            self.b_1 -= learning_rate * gradients_l1                                    # dL/db1 = dL/dz1 (hidden_size, 1)
+            self.W_1 -= learning_rate * np.dot(gradients_l1, np.transpose(x_i))         # dL/dW1 (hidden_size, n_features)
+        return loss / n_samples                                                         # normalize by n_samples
 
 
 def plot(epochs, train_accs, val_accs, filename=None):
